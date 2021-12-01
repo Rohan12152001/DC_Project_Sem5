0cbbdc1e779bdaf7779ff2d6693e4b9f2c51f7fa
Akash Yadav <akash.y0301@gmail.com>
2021-12-01 14:37:14 +0530

    update blog_form.html

--- s1/configuration.py
+++ s1/configuration.py
@@ -4,19 +4,22 @@ class DB:
     host = 'localhost'
     database = 'DcCovidData'
     user = 'root'
-    password = os.environ.get("Pass")
+    # password = os.environ.get("Pass")
+    password = "Ak@sh9920"
     secretKey = 'super secret key'
 
 class DB1:
     host = 'localhost'
     database = 'DcCovidData_Copy'
     user = 'root'
-    password = os.environ.get("Pass")
+    # password = os.environ.get("Pass")
+    password = "Ak@sh9920"
     secretKey = 'super secret key'
 
 class locksDB:
     host = 'localhost'
     database = 'lockDB'
     user = 'root'
-    password = os.environ.get("Pass")
+    # password = os.environ.get("Pass")
+    password = "Ak@sh9920"
     secretKey = 'super secret key'
